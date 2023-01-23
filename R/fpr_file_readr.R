a67ff117a1f7741041fdda3cd28a02cf71166831
hbelbo <Helmer.Belbo@nibio.no>
2023-01-23 06:26:38 +0100

    Adding parser for forwarder reports (.fpr)

---
+++ R/fpr_file_readr.R
@@ -0,0 +1,142 @@
+
+#' Fpr-file reader function
+#'
+#' @param fprfile filename and path of the fpr file to read
+
+#' @return A list of data.frames: stems, products, logs, machinereport_meta,
+#'  operators, objects, stem_grades, pricematrixes,
+#'  stemdiametervectors, and stemtypes.
+#' @export
+#'
+#' @examples
+#' fprfiles <- list.files(path =  system.file(package = "sf2010r"),
+#' pattern = ".fpr", recursive = TRUE, full.names= TRUE)
+#' fprtest1 <- fprdata(fprfiles[1])
+#' fprtest2 <- fprdata(fprfiles[2])
+#' fprtest3 <- fprdata(fprfiles[3])
+fprdata<- function(fprfile){
+  # fprfiles <- list.files(path =  system.file(package = "sf2010r"), pattern = ".fpr", recursive = TRUE, full.names= TRUE)
+  # fprfile = fprfiles[1]
+   doc <- xml2::read_xml(fprfile)
+   md5 <-  digest::digest(file(fprfile))
+
+   if(nchar(Sys.getlocale()) < 3){
+     Sys.setlocale(category = "LC_ALL", locale = "") #because of an R-studio Rstartup issue at HB's computer
+     }
+
+
+  ####
+  #Then validate the file is providing loads and other relevant info. This might be lacking in case the machine is reporting files automatically
+   LoadKey =  xml2::xml_integer(  xml2::xml_find_first(doc, ".//d1:LoadKey"))
+
+  if(length(LoadKey)){
+
+    ## then extract values
+
+    # .. cut object info
+    filename <- fprfile
+    tmp <- nchar(filename)
+    filetype <- substring(filename, tmp-2, tmp)
+
+
+    # .. from the header
+    MachineReportHeader <- sf2010r::getMachineReportHeader(doc)
+    # MachineReportHeader %>% dplyr::glimpse()
+
+    if(MachineReportHeader$diameterUnit[1]!="mm"){
+      print(paste(fprfile, ": Diameter Unit is NOT mm - please modify script for diasOB in Rfpr.R"))
+    }
+    TZ = fTZ(MachineReportHeader$CountryCode)
+
+    operators <- sf2010r::getOperators(doc)
+    # operators %>% dplyr::glimpse()
+
+    # Object definition (Harvest site)
+    objects <- sf2010r::getObjects( doc) # returns data_frame(ObjectKey, SubObjectKey, ObjectUserID, ObjectName, SubObjectName,  LoggingFormCode,  LoggingFormDesc )
+    # objects %>% dplyr::glimpse()
+
+
+    # Species and product definitions ----
+    cat(" -fprdata-getSpeciesGroupDefs- ")
+    speciesgroups <- sf2010r::getSpeciesGroupDefinitions(doc)
+    # speciesgroups %>% dplyr::glimpse()
+
+    cat(" -fprdata-getProductDefs \n")
+    products <- sf2010r::getProductDefs(doc) %>%
+      dplyr::mutate(  MachineKey = MachineReportHeader$MachineKey,
+             CreationDate = MachineReportHeader$CreationDate)
+    # products %>% dplyr::glimpse()
+
+
+    cat(" -fprdata-getStemTypes \n")
+    stemtypes <- sf2010r::getStemTypes(doc) %>%
+      mutate( MachineKey = MachineReportHeader$MachineKey)
+    # stemtypes %>% dplyr::glimpse()
+
+    cat(" -fprdata-getLocations \n")
+    locations <- sf2010r::getLocations(doc)
+
+    cat(" -fprdata-getDeliveries \n")
+    deliveries <- sf2010r::getDeliveries(doc)
+
+
+    ## forwarded loads  ----
+    cat(" -fprdata-getLoads \n")
+    Loads <- sf2010r::getLoads(doc)
+    Loads <- readr::type_convert(Loads)
+    # dplyr::glimpse(Loads)
+
+
+    ByLoad <-
+      Loads %>%
+      dplyr::group_by(MachineKey, LoadKey) %>%
+     dplyr::summarise(
+       dplyr::across(3:4, ~ dplyr::first(.x)),
+        dplyr::across(.cols = tidyselect::starts_with("Load_"), ~ sum(.x))
+      )
+
+
+    ByDelivery <-
+      Loads %>%
+      dplyr::group_by(MachineKey, DeliveryKey) %>%
+      dplyr::summarise(
+        dplyr::across(3:4, ~ dplyr::first(.x)),
+        dplyr::across(.cols = tidyselect::starts_with("Load_"), ~ sum(.x))
+      ) %>% dplyr::left_join( select(deliveries, tidyselect::starts_with("Delivery")), by = "DeliveryKey")
+
+    # Set up machine report table. One machine report = one observation. ----
+
+    machinereport_meta <- tibble::tibble(MachineKey = MachineReportHeader$MachineKey,
+                                    filename = filename,
+                                    file_md5 = md5,
+                                    CreationDate = MachineReportHeader$CreationDate,
+                                    object_keys = paste(objects$ObjectKey, collapse=", "),
+                                    object_ids = paste(objects$ObjectUserID, collapse=", "),
+                                    sub_obj_keys  = paste(objects$SubObjectKey, collapse=", "),
+                                    sub_obj_ids  = paste(objects$SubObjectUserID, collapse=", "),
+                                    filetype = filetype
+                                    )
+
+
+    #---------------
+
+  } else {
+    Loads = NULL
+    machinereport_meta = NULL
+    products = NULL
+    objects = NULL
+    Deliveries = NULL
+    ByLoad = ByDelivery = NULL
+
+  }
+
+  Ret <- list(Loads=Loads, Deliveries = deliveries, machinereport_meta =  machinereport_meta,
+              operators = operators, objects = objects,
+              ByLoad = ByLoad, ByDelivery = ByDelivery
+              )
+  return(Ret)
+}
+
+
+
+
--- R/fpr_functions.R
+++ R/fpr_functions.R
@@ -11,7 +11,7 @@
 #' plyr::ldply(locationlist[1], getLocation)
 getLocation <- function(x) {
   #   x = locationlist[[1]]
-
+  # fprfiles <- list.files(path =  paste0(getwd(), "/inst/extdata"),  pattern = ".fpr", ignore.case = TRUE,  recursive = TRUE, full.names= TRUE)
   chld.dat <- xml_childs_nchr(x)
 
   #gps <-  xml2::xml_find_all(x, "./d1:LocationCoordinates[@receiverPosition='Base machine position']") %>%
@@ -22,6 +22,28 @@ getLocation <- function(x) {
   chld.dat <- chld.dat %>% dplyr::mutate(dplyr::across(tidyselect::ends_with("Key"), as.integer))
   return(chld.dat)
 }
+
+
+#' Get location data for all locations within a SF2010 .fpr file
+#'
+#' @param doc a StanFord2010 .hpr xml-document
+#'
+#' @return a tibble
+#' @export
+#'
+#' @examples
+#' fprfiles <- list.files(path =  system.file(package = "sf2010r"), pattern = ".fpr", recursive = TRUE, full.names= TRUE)
+#' doc <- xml2::read_xml(fprfiles[1])
+#' getLocations(doc)
+getLocations <- function(doc){
+  locationlist <- xml2::xml_find_all(doc, ".//d1:LocationDefinition")
+  bmatrix <- plyr::ldply(locationlist, sf2010r::getLocation)
+  #bmatrix <- plyr::ldply(stemlist, getStemdata, ns = ns)
+  MachineKey <-xml2::xml_text(  xml2::xml_find_first(doc, ".//d1:MachineKey"))
+  bmatrix$MachineKey = MachineKey
+  return(bmatrix)
+}
+
 
 
 #' Delivery data from one delivery definition node
@@ -35,20 +57,129 @@ getLocation <- function(x) {
 #' nodelist <- xml2::xml_find_all(doc, ".//d1:DeliveryDefinition")
 #' getDelivery(nodelist[[1]]) %>% dplyr::glimpse()
 #' plyr::ldply(nodelist[1], getDelivery)
+#' plyr::ldply(nodelist, getDelivery)
   getDelivery <- function(x) {
   # x = nodelist[[1]]
 
   chld.dat <- xml_childs_nchr(x)
 
-  #gps <-  xml2::xml_find_all(x, "./d1:LocationCoordinates[@receiverPosition='Base machine position']") %>%
-  #  purrr::map_dfr( ~ sf2010r::xml_childs_nchr(.x)) %>%
-  #  dplyr::rename_with(~paste0(., "_bm"))
   # Then make the resulting tibble:
   chld.dat <- dplyr::bind_rows(chld.dat)
   chld.dat <- chld.dat %>% dplyr::mutate(dplyr::across(tidyselect::ends_with("Key"), as.integer))
   return(chld.dat)
 }
 
+  #' Get deliveries data for all deliveries within a SF2010 .fpr file
+  #'
+  #' @param doc a StanFord2010 .hpr xml-document
+  #'
+  #' @return a tibble
+  #' @export
+  #'
+  #' @examples
+  #' fprfiles <- list.files(path =  system.file(package = "sf2010r"), pattern = ".fpr", recursive = TRUE, full.names= TRUE)
+  #' doc <- xml2::read_xml(fprfiles[1])
+  #' getDeliveries(doc)
+  getDeliveries <- function(doc){
+    deliverieslist <- xml2::xml_find_all(doc, ".//d1:DeliveryDefinition")
+    bmatrix <- plyr::ldply(deliverieslist, sf2010r::getDelivery)
 
+
+    oneDelivry <-xml2::xml_find_first(doc, ".//d1:DeliveryDefinition")
+    plvsc <-xml2::xml_attr(xml2::xml_find_all(oneDelivry, ".//d1:Density"), attr = "densityCategory"  )
+    plvsc <- gsub('\\b(\\pL)\\pL{1,}|.','\\L\\1',plvsc,perl = TRUE) # Shortening the density categories
+    plvsc <- paste("Density_", plvsc, sep = "")
+    tst <- names(bmatrix)
+    tst[which( stringr::str_detect(tst, "Density" ) == TRUE)] <- plvsc
+    names(bmatrix) <- tst
+
+    #bmatrix <- plyr::ldply(stemlist, getStemdata, ns = ns)
+    MachineKey <-xml2::xml_text(  xml2::xml_find_first(doc, ".//d1:MachineKey"))
+    bmatrix$MachineKey = MachineKey
+    return(bmatrix)
+  }
+
+
+
+  #' Partial Load data from one Load  node
+  #' @param x is a node tree for one Load
+  #'
+  #' @export
+  #'
+  #' @examples
+  #' fprfiles <- list.files(path =  system.file(package = "sf2010r"),  pattern = ".fpr", ignore.case = TRUE,  recursive = TRUE, full.names= TRUE)
+  #' doc <- xml2::read_xml(fprfiles[1])
+  #' nodelist <- xml2::xml_find_all(doc, ".//d1:Load")
+  #' getPartialLoad(nodelist[[1]]) %>% dplyr::glimpse()
+  #' plyr::ldply(nodelist[1], getPartialLoad)
+  #' plyr::ldply(nodelist, getPartialLoad)
+  getPartialLoad <- function(x) {
+    # x = nodelist[[1]]
+    #NB: As loadVolumeCategories attributes seems equal across all partial loads in each fpr report, these are fetced once for all for the entire report in another function.
+    PartialLoads <- xml2::xml_find_all(x, ".//d1:PartialLoad")
+    chld.dat <- plyr::ldply(PartialLoads, xml_childs_nchr)
+    chld.dat <- dplyr::bind_rows(chld.dat)
+    chld.dat <- chld.dat %>% dplyr::mutate(dplyr::across(tidyselect::ends_with("Key"), as.integer))
+    return(chld.dat)
+  }
+
+
+
+
+  #' Load data from one Load  node
+  #' @param x is a node tree for one Load
+  #'
+  #' @export
+  #'
+  #' @examples
+  #' fprfiles <- list.files(path =  system.file(package = "sf2010r"),  pattern = ".fpr", ignore.case = TRUE,  recursive = TRUE, full.names= TRUE)
+  #' doc <- xml2::read_xml(fprfiles[1])
+  #' nodelist <- xml2::xml_find_all(doc, ".//d1:Load")
+  #' getLoad(nodelist[[1]]) %>% dplyr::glimpse()
+  #' plyr::ldply(nodelist[1], getLoad)
+  #' plyr::ldply(nodelist, getLoad)
+  getLoad <- function(x) {
+    # x = nodelist[[1]]
+
+    chld.dat <- xml_childs_nchr(x)
+    chld.dat <- dplyr::bind_rows(chld.dat)
+    chld.dat <- chld.dat %>% dplyr::mutate(dplyr::across(tidyselect::ends_with("Key"), as.integer))
+
+    partial_loads <- getPartialLoad(x)
+    partial_loads$LoadKey <- chld.dat$LoadKey
+    load_dt <- dplyr::right_join(chld.dat, partial_loads, by = "LoadKey")
+    return(load_dt)
+  }
+
+
+
+
+  #' Get load data for all loads within a SF2010 .fpr file
+  #'
+  #' @param doc a StanFord2010 .fpr xml-document
+  #'
+  #' @return a tibble
+  #' @export
+  #'
+  #' @examples
+  #' fprfiles <- list.files(path =  system.file(package = "sf2010r"), pattern = ".fpr", recursive = TRUE, full.names= TRUE)
+  #' doc <- xml2::read_xml(fprfiles[1])
+  #' getLoads(doc)
+  getLoads <- function(doc){
+    loadlist <- xml2::xml_find_all(doc, ".//d1:Load")
+    bmatrix <- plyr::ldply(loadlist, sf2010r::getLoad)
+    #bmatrix <- plyr::ldply(loadlist, getLoad)
+    onepartialload <- xml2::xml_find_first(loadlist[[1]], ".//d1:PartialLoad")
+    plvsc <-xml2::xml_attr(xml2::xml_find_all(onepartialload, ".//d1:LoadVolume"), attr = "loadVolumeCategory"  )
+    plvsc <- stringr::str_replace(ifelse(stringr::str_detect(plvsc, pattern = "m3sob|m3sub"), yes = plvsc, no = "Load_othervm"), pattern = "Volume, ", "Load_")
+
+    tst <- names(bmatrix)
+    tst[which( stringr::str_detect(tst, "LoadVolume.." ) == TRUE)] <- plvsc
+    names(bmatrix) <- tst
+
+    MachineKey <-xml2::xml_text(  xml2::xml_find_first(doc, ".//d1:MachineKey"))
+    bmatrix$MachineKey = MachineKey
+    return(bmatrix)
+  }
 
 
