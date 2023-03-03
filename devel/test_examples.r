6e9e0873e0bf04f123c3a0fee125be5d6c330798
wihe-HP <wilson.lara@nibio.no>
2023-03-03 10:52:42 +0100

    test_examples.r

--- devel/test_examples.r
+++ devel/test_examples.r
@@ -1,21 +1,36 @@
 require('sf2010r')
 
 hprfiles <- list.files(path =  system.file(package = "sf2010r"),
-pattern = ".hpr", recursive = TRUE, full.names= TRUE)
-hprtest1 <- hprdata(hprfiles[1])
-
-hprt <- Map(function(x)(hprdata(x)), hprfiles[1:2])
-
+ pattern = ".hpr", recursive = TRUE, full.names= TRUE)
+## hprtest1 <- hprdata(hprfiles[1])
+hprfiles <- list.files(path =  system.file(package = "sf2010r"),
+pattern = ".hpr$", recursive = TRUE, full.names= TRUE)
+hprtest2 <- hprdata(hprfiles[1])
+## hprt <- Map(function(x)(hprdata(x)), hprfiles[1:2])
 pth <- system.file(package = "sf2010r")
 momfiles <- list.files(pth,".mom$",recursive=TRUE,ignore.case=TRUE,full.names= TRUE)
 momtest1 <- getMom.all(momfiles[1])
 
+require('urltools')
+
+str(url_parse(hprfiles[1L]))
 
 
+pth <- hprfiles <- list.files(path =  system.file(package = "sf2010r"),
+ pattern = ".", recursive = TRUE, full.names= TRUE)
+get_url_ext <- function(URL)
+tools::file_ext(sub("\\?.+", "", URL))
+Map(function(x)
+    get_url_ext(x), pth)
 
-lapply(hprt[[1]], class)
+funcs <- c(mom = 'getMom.all',hpr = 'hpr_data')
+casein <- paste0('(?i)', names(funcs))
+collap <- paste(casein, collapse = '|')
+selurl <- pth[grepl(collap, pth)]
 
-do.call(rbind, hprtests1)
+fun_groups <- Map(function(x)
+    pth[grepl(x, pth)], casein)
+
 
 hprtest2 <- hprdata(hprfiles[2])
 hprtest3 <- hprdata(hprfiles[3])
