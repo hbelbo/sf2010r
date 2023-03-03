b4f5236064ca65087c12b445555403e79fed8d73
wihe-HP <wilson.lara@nibio.no>
2023-03-03 08:38:46 +0100

    test_examples.r

--- devel/test_examples.r
+++ devel/test_examples.r
@@ -4,6 +4,19 @@ hprfiles <- list.files(path =  system.file(package = "sf2010r"),
 pattern = ".hpr", recursive = TRUE, full.names= TRUE)
 hprtest1 <- hprdata(hprfiles[1])
 
+hprt <- Map(function(x)(hprdata(x)), hprfiles[1:2])
+
+pth <- system.file(package = "sf2010r")
+momfiles <- list.files(pth,".mom$",recursive=TRUE,ignore.case=TRUE,full.names= TRUE)
+momtest1 <- getMom.all(momfiles[1])
+
+
+
+
+lapply(hprt[[1]], class)
+
+do.call(rbind, hprtests1)
+
 hprtest2 <- hprdata(hprfiles[2])
 hprtest3 <- hprdata(hprfiles[3])
 
@@ -15,11 +28,17 @@ hprtests <- purrr::map_dfr(hprfiles, ~tibble::tibble(filename = .x,
 str(hprtests)
 
 
+system.time(
+)
+
+lapply(hprtests1, tail)
+
 
 system.time(
 hprtests1 <- Map(function(x)tibble::tibble(hprdata(x)), hprfiles[1:2])
 )
 
 str(hprtests1)
+
 
 
