3f8357d0a80b1ac2c29378ceb259f35c23437af5
wihe-HP <wilson.lara@nibio.no>
2023-03-03 13:02:36 +0100

    test_examples.r

--- devel/test_examples.r
+++ devel/test_examples.r
@@ -1,4 +1,33 @@
 require('sf2010r')
+pth <- hprfiles <- list.files(path =  system.file(package = "sf2010r"),
+ pattern = ".", recursive = TRUE, full.names= TRUE)
+
+get_Harv_data <- function(pth,
+                          funcs = c(mom = 'getMom.all',hpr = 'hprdata')){
+    casein <- paste0('(?i)', names(funcs))
+    collap <- paste(casein, collapse = '|')
+    selurl <- pth[grepl(collap, pth)]
+    fun_groups <- Map(function(x)
+        pth[grepl(x, pth)], casein)
+    names(fun_groups) <- funcs
+    nmfns <- sapply(fun_groups, length)
+    fns2map <- Map(function(x,y)
+        rep(x,y), funcs, nmfns)
+    unl_nmfns <- unlist(fns2map, use.names = FALSE)
+    unl_fun_groups <- unlist(fun_groups, use.names = FALSE)
+    out <- Map(function(x,y)
+        tryCatch(do.call(x, list(y)), error = function(e)NULL),
+        unl_nmfns, unl_fun_groups)
+    names(out) <- unl_fun_groups
+    return(out)}
+
+pth
+tmp <- get_Harv_data(pth)
+
+names(tmp)
+length(tmp)
+
+
 
 hprfiles <- list.files(path =  system.file(package = "sf2010r"),
  pattern = ".hpr", recursive = TRUE, full.names= TRUE)
@@ -23,13 +52,36 @@ tools::file_ext(sub("\\?.+", "", URL))
 Map(function(x)
     get_url_ext(x), pth)
 
-funcs <- c(mom = 'getMom.all',hpr = 'hpr_data')
+funcs <- c(mom = 'getMom.all',hpr = 'hprdata')
 casein <- paste0('(?i)', names(funcs))
 collap <- paste(casein, collapse = '|')
 selurl <- pth[grepl(collap, pth)]
-
 fun_groups <- Map(function(x)
     pth[grepl(x, pth)], casein)
+names(fun_groups) <- funcs
+nmfns <- sapply(fun_groups, length)
+fns2map <- Map(function(x,y)
+    rep(x,y), funcs, nmfns)
+unl_nmfns <- unlist(fns2map, use.names = FALSE)
+unl_fun_groups <- unlist(fun_groups, use.names = FALSE)
+out <- Map(function(x,y)
+    tryCatch(do.call(x, list(y)), error = function(e)NULL), unl_nmfns, unl_fun_groups)
+names(out) <- unl_fun_groups
+
+str(out)
+
+    
+    rep(names(x), length(x)), fun_groups)
+
+do.call(names(fun_groups)[1L])
+
+
+temp <- Map(function(x)
+    do.call(fun.,x), tmp[[1]])
+
+tmp <- fun_groups[[2L]][1L]
+
+
 
 
 hprtest2 <- hprdata(hprfiles[2])
