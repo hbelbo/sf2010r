46e6147870b56e374a8ece7bbce132d09a70b5e5
Helmer Belbo <Helmer.Belbo@nibio.no>
2023-08-04 19:01:11 +0200

    improving fpr_file_readr

--- R/fpr_file_readr.R
+++ R/fpr_file_readr.R
@@ -28,6 +28,7 @@ fpr_file_readr <- function(fprfile){
   #Then validate the file is providing loads and other relevant info. This might be lacking in case the machine is reporting files automatically
    LoadKey =  xml2::xml_integer(  xml2::xml_find_first(doc, ".//d1:LoadKey"))
 
+
   if(length(LoadKey)){
 
     ## then extract values
@@ -39,13 +40,13 @@ fpr_file_readr <- function(fprfile){
 
 
     # .. from the header
-    MachineReportHeader <- sf2010r::getMachineReportHeader(doc)
-    # MachineReportHeader %>% dplyr::glimpse()
+    header <- sf2010r::getMachineReportHeader(doc)
+    # header %>% dplyr::glimpse()
 
-    if(MachineReportHeader$diameterUnit[1]!="mm"){
+    if(header$diameterUnit[1]!="mm"){
       print(paste(fprfile, ": Diameter Unit is NOT mm - please modify script for diasOB in Rfpr.R"))
     }
-    TZ = fTZ(MachineReportHeader$CountryCode)
+    TZ = fTZ(header$CountryCode)
 
     operators <- sf2010r::getOperators(doc)
     # operators %>% dplyr::glimpse()
@@ -54,45 +55,67 @@ fpr_file_readr <- function(fprfile){
     objects <- sf2010r::getObjects( doc) # returns data_frame(ObjectKey, SubObjectKey, ObjectUserID, ObjectName, SubObjectName,  LoggingFormCode,  LoggingFormDesc )
     # objects %>% dplyr::glimpse()
 
+    returnlist <- list(header = header, operators = operators,  objects = objects)
 
     # Species and product definitions ----
     cat(" -fpr_file_readr-getSpeciesGroupDefs- ")
     speciesgroups <- sf2010r::getSpeciesGroupDefinitions(doc)
     # speciesgroups %>% dplyr::glimpse()
+    if(length(speciesgroups)){
+      speciesgroups <- c(returnlist, speciesgroups = list(speciesgroups))
+    }
+
 
     cat(" -fpr_file_readr-getProductDefs \n")
-    products <- sf2010r::getProductDefs(doc) %>%
-      dplyr::mutate(  MachineKey = MachineReportHeader$MachineKey,
-             CreationDate = MachineReportHeader$CreationDate)
-    # products %>% dplyr::glimpse()
+    products <- sf2010r::getProductDefs(doc)
+    if(length(products)){
+      products <- products %>%
+      dplyr::mutate(  MachineKey = header$MachineKey
+                    #  , CreationDate = header$CreationDate
+             )
+      returnlist <- c(returnlist, products = list(products))
+    }
 
 
     cat(" -fpr_file_readr-getStemTypes \n")
-    stemtypes <- sf2010r::getStemTypes(doc) %>%
-      mutate( MachineKey = MachineReportHeader$MachineKey)
-    # stemtypes %>% dplyr::glimpse()
+    stemtypes <- sf2010r::getStemTypes(doc)
+    if(length(stemtypes)){
+      stemtypes <- stemtypes %>% mutate( MachineKey = header$MachineKey)
+      returnlist <- c(returnlist, stemtypes = list(stemtypes))
+    }
 
     cat(" -fpr_file_readr-getLocations \n")
     locations <- sf2010r::getLocations(doc)
+    if(length(locations)){
+      returnlist <- c(returnlist, locations = list(locations))
+    }
+
 
     cat(" -fpr_file_readr-getDeliveries \n")
     deliveries <- sf2010r::getDeliveries(doc)
+    if(length(deliveries)){
+      returnlist <- c(returnlist, deliveries = list(deliveries))
+    }
 
 
     ## forwarded loads  ----
     cat(" -fpr_file_readr-getLoads \n")
     Loads <- sf2010r::getLoads(doc)
     Loads <- readr::type_convert(Loads)
+    returnlist <- c(returnlist, Loads = list(Loads))
+
+
     # dplyr::glimpse(Loads)
 
 
     ByLoad <-
       Loads %>%
-      dplyr::group_by(.data$MachineKey, LoadKey) %>%
+      dplyr::group_by(.data$MachineKey, .data$LoadKey, .data$LocationKey) %>%
      dplyr::summarise(
        dplyr::across(3:4, ~ dplyr::first(.x)),
         dplyr::across(.cols = tidyselect::starts_with("Load_"), ~ sum(.x))
       )
+    returnlist <- c(returnlist, ByLoad = list(ByLoad))
 
 
     ByDelivery <-
@@ -103,37 +126,30 @@ fpr_file_readr <- function(fprfile){
         dplyr::across(.cols = tidyselect::starts_with("Load_"), ~ sum(.x))
       ) %>% dplyr::left_join( select(deliveries, tidyselect::starts_with("Delivery")), by = "DeliveryKey")
 
+    returnlist <- c(returnlist, ByDelivery = list(ByDelivery))
+
     # Set up machine report table. One machine report = one observation. ----
 
-    machinereport_meta <- tibble::tibble(MachineKey = MachineReportHeader$MachineKey,
+    machinereport_meta <- tibble::tibble(MachineKey = header$MachineKey,
                                     filename = filename,
                                     file_md5 = md5,
-                                    CreationDate = MachineReportHeader$CreationDate,
+                                    CreationDate = header$CreationDate,
                                     object_keys = paste(objects$ObjectKey, collapse=", "),
                                     object_ids = paste(objects$ObjectUserID, collapse=", "),
                                     sub_obj_keys  = paste(objects$SubObjectKey, collapse=", "),
                                     sub_obj_ids  = paste(objects$SubObjectUserID, collapse=", "),
                                     filetype = filetype
                                     )
+    returnlist <- c(returnlist, machinereport_meta = list(machinereport_meta))
 
 
     #---------------
 
   } else {
-    Loads = NULL
-    machinereport_meta = NULL
-    products = NULL
-    objects = NULL
-    Deliveries = NULL
-    ByLoad = ByDelivery = NULL
-
+  returnlist = NULL
   }
 
-  Ret <- list(Loads=Loads, Deliveries = deliveries, machinereport_meta =  machinereport_meta,
-              operators = operators, objects = objects,
-              ByLoad = ByLoad, ByDelivery = ByDelivery
-              )
-  return(Ret)
+  return(returnlist)
 }
 
 
